99e0d3e36197839e4496f512fa92e2c5ed17ac4c
Ivan <romanomess@gmail.com>
2021-05-02 10:48:22 +0300

    checkAuth fixes

--- utils/checkAuth.ts
+++ utils/checkAuth.ts
@@ -1,12 +1,17 @@
+import { Store } from "@reduxjs/toolkit";
 import { GetServerSidePropsContext } from "next";
 import { Api } from "../api";
 import { UserData } from "../pages";
+import { setUserData } from "../redux/slices/userSlice";
+import { RootState } from "../redux/types";
 
 export const checkAuth = async (
-  ctx: GetServerSidePropsContext
+  ctx: any & { store: Store<RootState> }
 ): Promise<UserData | null> => {
   try {
-    return await Api(ctx).getMe();
+    const user = await Api(ctx).getMe();
+    ctx.store.dispatch(setUserData(user));
+    return user;
   } catch (err) {
     return null;
   }
